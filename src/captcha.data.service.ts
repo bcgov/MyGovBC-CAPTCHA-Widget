0f8e1d855b6bb2d4f43213f53d8e13548e0bd0e7
Greg Turner <greg.fun@gmail.com>
2017-02-21 15:32:44 -0800

    added test harness around component

--- src/captcha.data.service.ts
+++ src/captcha.data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import {Observable} from "rxjs";
+import 'rxjs/Rx';
 
 @Injectable()
 export class DataService {
---
+++ test/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+
+})
+
+export class AppComponent {
+  title = 'MyGovBC Captcha Widget Client Example';
+  api = "http://localhost:3000";
+}
