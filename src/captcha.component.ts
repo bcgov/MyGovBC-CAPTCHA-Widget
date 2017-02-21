7e019137e7614d3ae57df73026ed7fb877d90364
Greg Turner <greg.fun@gmail.com>
2017-02-21 14:05:38 -0800

    switched to in-line template

--- src/captcha.component.ts
+++ src/captcha.component.ts
@@ -1,71 +1,102 @@
 import {Component, ElementRef, ViewChild, Output, Input} from '@angular/core';
-import { DataService } from './captcha.data.service';
+import {DataService} from './captcha.data.service';
 
 @Component({
-	selector: 'captcha',
-	template: require('./captcha.component.html'),
-	styles: [require('./captcha.component.css')],
-	providers: [DataService]
+  selector: 'captcha',
+  template: `<div class="container">
+  <div class="form-group row">
+    <div class="col-sm-4">
+      <label>Captcha Image:</label>
+      <div #image></div>
+      <button class="btn btn-success" (click)="getNewCaptcha()">Refresh</button>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <form>
+    <div class="form-group row">
+      <div class="col-sm-4">
+        <label for="answer">Enter The Text You See Above:</label>
+        <input
+          type="text"
+          class="form-control"
+          id="answer"
+          [(ngModel)]="answer"
+          name="answer"
+          required>
+      </div>
+    </div>
+    <div class="form-group">
+      <button type="submit" class="btn btn-success">Submit</button>
+    </div>
+  </form>
+  <div class="form-group row">
+    <div *ngIf="captchaValid != null && captchaValid != false" class="col-sm-4 glyphicon glyphicon-ok"> Correct!</div>
+    <div *ngIf="captchaValid == false" class="col-sm-4 glyphicon glyphicon-remove"> Incorrect Answer!</div>
+  </div>
+</div>
+`,
+  providers: [DataService]
 })
 
 export class CaptchaComponent {
 
-  @Input('apiBaseUrl') apiBaseUrl:string;
-  @Input('nonce') nonce:string;
-  @Output('valid') captchaValid:boolean = null;
-	@Output('token') jwt: string;
-	validation 		= "";
-	answer 				= "";
+  @Input('apiBaseUrl') apiBaseUrl: string;
+  @Input('nonce') nonce: string;
+  @Output('valid') captchaValid: boolean = null;
+  @Output('token') jwt: string;
+  validation = "";
+  answer = "";
 
-	@ViewChild('image') imageContainer: ElementRef;
+  @ViewChild('image') imageContainer: ElementRef;
 
-	constructor(private dataService: DataService, private element: ElementRef) {
-		this.getNewCaptcha(false);
-	}
+  constructor(private dataService: DataService, private element: ElementRef) {
+    this.getNewCaptcha(false);
+  }
 
-	// Handle form submission
-	public onSubmit() {
-		// Attempt to validate the user's token.
-		// console.log("onsubmit", this.answer);
+  // Handle form submission
+  public onSubmit() {
+    // Attempt to validate the user's token.
+    // console.log("onsubmit", this.answer);
 
-		this.dataService.verifyCaptcha(this.apiBaseUrl, this.nonce, this.answer, this.validation).subscribe(
-			(res) => this.handleVerify(res)
-			);
-	}
+    this.dataService.verifyCaptcha(this.apiBaseUrl, this.nonce, this.answer, this.validation).subscribe(
+      (res) => this.handleVerify(res)
+    );
+  }
 
-	// Call the backend to see if our answer is correct
-	private handleVerify(payload) {
-		// console.log("payload response:", payload);
+  // Call the backend to see if our answer is correct
+  private handleVerify(payload) {
+    // console.log("payload response:", payload);
 
-		if (payload.valid === true) {
-			this.captchaValid = true;
-			this.jwt = payload.jwt;
-		} else {
-			this.captchaValid = false;
-			// They failed - try a new one.
-			this.getNewCaptcha(true);
-		}
-	}
+    if (payload.valid === true) {
+      this.captchaValid = true;
+      this.jwt = payload.jwt;
+    } else {
+      this.captchaValid = false;
+      // They failed - try a new one.
+      this.getNewCaptcha(true);
+    }
+  }
 
-	public getNewCaptcha(errorCase) {
-		console.log("getting new captcha.");
-		// Reset things
-		this.jwt = "";
-		if (!errorCase) {
-			// Let them know they failed instead of wiping out the answer area
-			// Contructing this form on page load/reload will have errorCase = false
-			this.captchaValid = null;
-		}
-		this.dataService.fetchData(this.apiBaseUrl, this.nonce).subscribe(
-			(res) => this.handleCaptcha(res)
-			);
-	}
+  public getNewCaptcha(errorCase) {
+    console.log("getting new captcha.");
+    // Reset things
+    this.jwt = "";
+    if (!errorCase) {
+      // Let them know they failed instead of wiping out the answer area
+      // Contructing this form on page load/reload will have errorCase = false
+      this.captchaValid = null;
+    }
+    this.dataService.fetchData(this.apiBaseUrl, this.nonce).subscribe(
+      (res) => this.handleCaptcha(res)
+    );
+  }
 
-	// We received a payload from the server - apply it to our form.
-	private handleCaptcha(payload) {
-		// console.log("payload:", payload);
+  // We received a payload from the server - apply it to our form.
+  private handleCaptcha(payload) {
+    // console.log("payload:", payload);
 
-		this.imageContainer.nativeElement.innerHTML = payload.captcha;
-		this.validation = payload.validation;
-	}
+    this.imageContainer.nativeElement.innerHTML = payload.captcha;
+    this.validation = payload.validation;
+  }
 }
