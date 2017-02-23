6b0b6320140ef52f6dc193ca63d338c7ac3ea987
Greg Turner <greg.fun@gmail.com>
2017-02-23 14:03:14 -0800

    looks like ES5 now

--- lib/captcha.data.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Http, Response } from '@angular/http';
-import { Observable } from "rxjs";
-import 'rxjs/Rx';
-export declare class DataService {
-    private http;
-    constructor(http: Http);
-    fetchData(apiBaseUrl: string, nonce: string): Observable<Response>;
-    verifyCaptcha(apiBaseUrl: string, nonce: string, answer: string, encryptedAnswer: string): Observable<Response>;
-}
